1e389b329fdbab2e79b66fa8b58d7948adfb54c7
amir23060 <amir23060@gmail.com>
2025-10-13 16:04:10 +0200

    .

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SignUp } from "./LoginAndSignup/signup";
+export default function App (){
+
+  return(<>
+  <SignUp/>
+  </>)
+}
---
+++ Frontend/src/LoginAndSignup/signup.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./signup.css";
+export function SignUp() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = "";
+  return (
+    <>
+      <div className="container">
+        <div className="inputs">
+            
+        </div>
+        <div className="image"></div>
+      </div>
+    </>
+  );
+}
