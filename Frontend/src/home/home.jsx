b0e563b0e96f72986188bb384e14abb9c4771a2c
amir23060 <amir23060@gmail.com>
2025-10-15 18:37:44 +0200

    fixed

--- Frontend/src/home/home.jsx
+++ Frontend/src/home/home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "./home.css"
 
 import { Visited } from "../components/visited/visited";
+import { Navbar } from "../components/navbar/navbar";
 export function Home(){
 
     return (
       <>
+      <Navbar/>
         <div className="headlineImage">
           <p>Restaurant Tracker </p>
         </div>
