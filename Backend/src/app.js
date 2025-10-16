20718dd19a5b3c4fcdc8864d95d7c26aa3548c2c
Reman Rada <kleamy@MacBook-Air-som-tillhor-Reman.local>
2025-10-16 15:23:30 +0200

    blev chorba i merge conflict men löste det och skickade in denna nu efter fix.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,3 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", restaurantRoutes);
+
+app.get("/", (req, res) => {
+    res.json({ ok: true });
+});
+
+export default app;
