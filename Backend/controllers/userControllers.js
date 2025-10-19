edce56767ed00badbded421e851136fc8effdd8c
amir23060 <amir23060@gmail.com>
2025-10-19 15:38:53 +0200

    made authoration for user controllers

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -1,35 +1,72 @@
 import express from "express"
 import User from "../model/userModel.js"
+import bcrypt from "bcrypt"
 export const addUser=async(req,res)=>{
   const {firstName,lastName,email,password }=req.body
-  const newUser= new User({firstName,lastName,email,password})
+
+ try{
+  const existingUser= await User.findOne({email})
+  if(existingUser){
+    return res.status(400).json({message:"email already in use ,try logging in  "})
+  }
+  const hashedPassword =await bcrypt.hash(password ,10)
+  const newUser= new User({firstName,lastName,email,password:hashedPassword})
   await newUser.save()
-  res.json(newUser)
+   const token = jwt.sign({ id: newUser._id, email: newUser.email }, JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    const { password: _, ...userWithoutPassword } = newUser.toObject();
+
+    return res.status(201).json({
+      user: userWithoutPassword,
+      token,
+      message: "Account successfully created",
+    });
+
+ }catch(error){
+  console.error("Error adding user:", error);
+   return res.status(500).json({message:"connot add a new user"})
+ }
 }
 export const getUser=async(req,res)=>{
 const {email,password}=req.body
     const user = await User.findOne({ email });
+try{
+    const existingUser= await User.findOne({email})
+    if(!existingUser){
+      return res.status(400).json({message:"Incorrect email"})
+    }
+    const isPasswordValid= await bcrypt.compare(password,existingUser.password)
+    if(!isPasswordValid){
+      return res.status(400).json({message:"incorrect email or password"})
+      
+    }
+     const token = jwt.sign({ id: newUser._id, email: newUser.email }, JWT_SECRET, {
+      expiresIn: "7d",
+    });
+        const { password: _, ...userWithoutPassword } = newUser.toObject();
+   return res.status(200).json({
+      user: userWithoutPassword,
+      token,
+      message: "Login successful",
+    });
 
-    if (!user) {
-      return res.status(400).json({ message: "Invalid email or password" });
-    }
-    if(user.password === password){
-        res.json(user)
-    }
-    else{
-        res.status(404).json({message:"failed"})
-    }
-
+}catch(error){
+  console.log(error)
+  return res.status(500).json({message:"something went wrong"})
+}
 
 }
 export const getUserById=async(req,res)=>{
     const {id}= req.params
   try{
-    const user = await User.findById(id)
-    if(user){
-        res.json(user)
+    const user = await User.findById(id).select("-password")
+    if(!user){
+      res.status(404).json({ message: "User not found" });
+       
     }
-  res.status(404).json({ message: "User not found" });
+ res.status(200).json(user)
 
   }
   catch(error){
